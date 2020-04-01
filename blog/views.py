5459ff1de1533d5790c3a5c9df39e613236f3414
James Oh <taekhwan.oh@nhrec.org>
2020-04-01 01:38:55 +0000

    Added post in the blog model, changed design

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'James Oh',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 29, 2020'
-    },
-    {
-        'author': 'Boo',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 30, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

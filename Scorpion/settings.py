4e223fbd761f208ec927964ae0d851254fc24aaa
James Oh <taekhwan.oh@nhrec.org>
2020-04-12 11:01:48 +0000

    Finished profile page and updating the profile

--- Scorpion/settings.py
+++ Scorpion/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media' )    #directory that uploaded file will be saved
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'    #Change bootsrap setting to bootstrap4
 
 LOGIN_REDIRECT_URL = 'blog-home'
